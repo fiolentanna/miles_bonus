6bcd57ece27b9cfad7bb0203ab65eb4900ebaa41
fiolentanna <tigranka@gmail.com>
2021-04-06 23:58:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args) {
+        long TicketPrice = 1050;
+        int milePrice = 20;
+
+        long miles_amount = TicketPrice / milePrice;
+        System.out.println(miles_amount);
+    }
+
+}
