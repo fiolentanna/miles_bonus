df5203d9532dda4332f3b6a2c2e40bd0200a1209
fiolentanna <tigranka@gmail.com>
2021-04-07 00:09:15 +0300

    code edits

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main (String[] args) {
-        long TicketPrice = 1050;
+        long ticketPrice = 1050;
         int milePrice = 20;
 
-        long miles_amount = TicketPrice / milePrice;
-        System.out.println(miles_amount);
+        long milesAmount = ticketPrice / milePrice;
+        System.out.println(milesAmount);
     }
 
 }
